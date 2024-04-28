c4d00537c202525e4e9106dfafd9d998a22fad28
vhle12 <158373104+vhle12@users.noreply.github.com>
2024-04-28 16:47:24 -0400

    complete javacript continued lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(work = 'go to the office') {
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(highlight){
+    return function(adjective){
+        return `You are ${highlight}${adjective}${highlight}!`
+    }
+}
